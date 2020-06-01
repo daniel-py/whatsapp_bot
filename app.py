7ad7318b54a47de55634e75a309c443f2b82cde3
Daniel <doluremi19@gmail.com>
2020-06-02 00:54:05 +0100

    Another one

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 from investment_project import *
 
 check = 0
@@ -34,12 +35,77 @@ def sms_reply():
     		
     
     if check == 1:
+    	check += 1
 	    create_mou(msg, 127000)
 	    resp.message("Here's your MOU")
 	    resp.message().media(f"files/{msg.upper().center(1) + ' MOU'}.pdf")
 
 	    return str(resp)
 
+	if 'i am the ceo' in msg or 'i\'m the ceo' in msg:
+	    if msg.split()[-1] == 's3cr3tpassw0rd':
+	        check = 'approved'
+	     	resp.message("Oh, good day Mr. Daniel.\nPlease enter one of the following options (only) to access its functionality: 
+	     		\n*A.* Add new investor. 
+	     		\n*B.* Send in the database.
+	     		\n*C.* How many investors have I had?
+	     		\n*D.* How much has been invested with me?
+	     		\n*E.* How much ROI has been generated totally?
+	     		\n*F.* Who is my biggest investor?
+	     		\n*G.* Show me some information about amounts invested over the months.
+	     		\n*H.* Show me some information about the number of investors I've had over the months.
+	     		\n*I.* Show me some information about the ROIs generated over the months.")
+	     	return str(resp)
+
+	    else:
+	     	resp.message("Your password is incorrect. Please try again.")
+	     	return str(resp)
+
+
+	if check == 'approved' and 'A' in msg.split()[0]:
+		if os.path.isfile("files/approved_investor.json") and os.stat("files/approved_investor.json").st_size != 0:
+		    old_file = open("files/approved_investor.json", "r+")
+		    data = json.loads(old_file.read())
+		    data['investor_detail'] = ' '.join(msg.split()[1:])
+		else:
+		    old_file = open("files/approved_investor.json", "w+")
+		    data = {'investor_detail': '+2347086643074, 150000'}
+		    
+		old_file.seek(0)
+		old_file.write(json.dumps(data))
+
+		#export TWILIO_ACCOUNT_SID = 'ACbad3bf98ea7d52112b152e8d8222efb4'
+		#export TWILIO_AUTH_TOKEN = 'b8866b872d2950e651fadc1426463b5c'
+
+
+		client = Client()
+
+		fromm = 'whatsapp:+14155238886'
+		to = 'whatsapp:' + data['investor_detail'].split(', ')[0]
+
+		client.messages.create(body = "Hello. Your payment has been approved.\nPlease send your name in this format for your MOU:\n*Mr./Mrs. Firstname Middlename Lastname*", from_ = fromm, to = to)
+
+
+		check = 'name'
+
+	if check == 'name':
+		old_file = open("files/approved_investor.json", "r+")
+		data = json.loads(old_file.read())
+		if number == data['investor_detail'].split(', ')[0]:
+			create_mou(msg, data['investor_detail'].split(', ')[-1])
+			resp.message("Here's your MOU")
+		    resp.message().media(f"files/{msg.upper().center(1) + ' MOU'}.pdf")
+
+		    return str(resp)
+
+
+	if check == 'approved' and 'B' in msg.split()[0]:
+		resp.message("Okay.")
+		resp.message().media("files/Database.xlsx")
+		return str(resp)
+
+	if check == 'approved' and 'C' in msg.split()[0]:
+		answer = more_functionalities(1, msg.split()[-1].replace(' ', ''))
 
 
     # Create reply
--- investment_project.py
+++ investment_project.py
@@ -127,7 +127,7 @@ def create_mou(name, capital):
              
 
              
-    wb = opxl.load_workbook("files/Book1 - Copy.xlsx")
+    wb = opxl.load_workbook("files/Database.xlsx")
     sheet = wb['Sheet1']
     max_row = sheet.max_row
     sheet.cell(max_row+1, 1).value = 'JI-'+str(int(sheet.cell(max_row, 1).value[3:]) + 1)
@@ -144,11 +144,125 @@ def create_mou(name, capital):
         else:
             l += 1
             
-    wb.save("files/Book1 - Copy.xlsx")
+    wb.save("files/Database.xlsx")
          
 
-#create_mou('Mr. Oluwabuba Victor', 120000)         
+#create_mou(input("Please input the name of the investor:  "), int(input("Amount invested:  ")))         
 #df = pd.read_csv("files/New.csv")
 #df.loc[df.index[-1]+1] = ['JI-'+str(int(df.iloc[-1][0][3:]) + 1), name, comma_separate(capital), 
 #                          ", ".join(date), comma_separate(int(int(capital) * 0.2)), comma_separate(roi), pay_date[1]]
 #df.to_csv('files/New.csv', index= False)
+
+
+def more_functionalities(x, year):
+
+    wb = opxl.load_workbook("files/Database.xlsx")
+    sheet = wb['Sheet1']
+    x = int(x)
+    #print("\n")
+
+    def string_to_num(b):
+        b = str(b)
+        b = b.replace(',', '')
+        return int(b)
+
+    df = pd.read_excel("files/Database.xlsx", skiprows=[x for x in range(0, 8)])
+    df["AMOUNT INVESTED(₦)"] = df["AMOUNT INVESTED(₦)"].apply(string_to_num)
+    df["ROI DUE(₦)"] = df["ROI DUE(₦)"].apply(string_to_num)
+    df["ROI + CAPITAL(₦)"] = df["ROI + CAPITAL(₦)"].apply(string_to_num)
+    capitals = np.array(df["AMOUNT INVESTED(₦)"])
+    rois = np.array(df["ROI DUE(₦)"])
+
+    if x == 5 or x == 6 or x == 7:
+        #year = input("What year do you want to check?  ").replace(' ', '')
+        #print("\n")
+        
+        def in_a_year(a):
+            return a[-4:] == year
+              
+        a, b, c, d, e, f, g, h, i, j, k, l = ([], [], [], [], [], [], [], [], [], [], [], [])
+
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+                 'September', 'October', 'November', 'Decemeber']
+
+        lists = [a, b, c, d, e, f, g, h, i, j, k, l]
+              
+        
+
+        new_df = df[df["TIME INVESTED"].apply(in_a_year)].reset_index().drop("index", axis = 1)
+
+        m = 0
+        for i in new_df["TIME INVESTED"]:
+            for n, o in zip(months, lists):
+                if n in i:
+                    o.append(new_df.loc[m]["AMOUNT INVESTED(₦)"])
+            m+= 1
+
+
+        
+        a, b, c, d, e, f, g, h, i, j, k, l = ([], [], [], [], [], [], [], [], [], [], [], [])
+
+        
+        roilists = [a, b, c, d, e, f, g, h, i, j, k, l]
+        roi_df = df[df["PAY DATE"].apply(in_a_year)].reset_index().drop("index", axis = 1)
+
+        m = 0
+        for i in new_df["PAY DATE"]:
+            for n, o in zip(months, roilists):
+                if n in i:
+                    o.append(new_df.loc[m]["ROI DUE(₦)"])
+            m+= 1
+            
+            
+            
+    if x == 1:
+        return f"You have had {sheet.cell(sheet.max_row, 1).value[3:]} investor(s)"
+        
+    elif x == 2:
+        return f"{num2words(capitals.sum()).title()} Naira (₦{comma_separate(capitals.sum())})"
+
+    elif x == 3:
+        return f"{num2words(rois.sum()).title()} Naira (₦{comma_separate(rois.sum())})"
+        
+    elif x == 4:
+        a = list(capitals)
+        index = a.index(max(a))
+        return f"{df.loc[index]['NAME']} with a capital of {num2words(a[index]).upper()} NAIRA (₦{comma_separate(a[index])})"
+
+    elif x == 5:      
+        for n, o in zip(months, [np.sum(x) for x in lists]):
+            print(f"₦{comma_separate(o)} was invested in {n}")
+
+        plt.figure(figsize = (12,10))
+        plt.bar([x[0:3] for x in months], [np.sum(x) for x in lists], color = (.5, 0, 0))
+        plt.xlabel(f"Months of the year {year}")
+        plt.ylabel("Amount invested(₦)") 
+        #plt.title(f"Amount invested per month in the year {year}")
+
+        plt.savefig("Investments_per_month.png")
+        #print("\nThis image has been saved in your 'Investment_project' folder")
+              
+    elif x == 6:
+        for n, o in zip(months, lists):
+            print(f"{len(o)} investor(s) came in {n}")
+        
+        plt.figure(figsize = (12,10))
+        plt.xlabel(f"Months of the year {year}")   
+        plt.ylabel("Number of investors") 
+        plt.bar([x[:3] for x in months], [len(x) for x in lists], color = (0, 0, 0))
+
+        plt.savefig("Investors_per_month.png")
+        print("\nThis image has been saved in your 'Investment_project' folder")
+              
+    elif x == 7:
+        for n, o in zip(months, [np.sum(x) for x in roilists]):
+            print(f"₦{comma_separate(o)} was generated in {n}")
+
+        plt.figure(figsize = (12,10))
+        plt.bar([x[0:3] for x in months], [np.sum(x) for x in roilists], color = (0, 0, .5))
+        plt.xlabel(f"Months of the year {year}")
+        plt.ylabel("ROIs generated(₦)") 
+        #plt.title(f"Amount generated per month in the year {year}")
+
+        plt.savefig("ROI_per_month.png")
+        print("\nThis image has been saved in your 'Investment_project' folder")
